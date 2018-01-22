9c9fbb5a7d81a6a3991aa5c5389a83520fac3e4d
Salvador Sastre <chavosastre@gmai.com>
2018-01-22 11:22:42 -0600

    27.-Introducción al Scaffolding  Controladores y Vistas

---
+++ CursoASPNETMVC/Models/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoASPNETMVC.Models
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public bool EstaEnCartelera { get; set; }
+        public string Genero { get; set; }
+
+    }
+}
